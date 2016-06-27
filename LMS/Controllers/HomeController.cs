7063e26fdebe4eed804a14b42753ba42826ddf40
kaffeutvecklare <kaffeutvecklare@gmail.com>
2016-06-27 10:14:20 +0200

    Added a Dispose-method to HomeController

--- LMS/Controllers/HomeController.cs
+++ LMS/Controllers/HomeController.cs
@@ -149,5 +149,25 @@ namespace LMS.Controllers
 
             return View();
         }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_userManager != null)
+                {
+                    _userManager.Dispose();
+                    _userManager = null;
+                }
+
+                if (_signInManager != null)
+                {
+                    _signInManager.Dispose();
+                    _signInManager = null;
+                }
+            }
+
+            base.Dispose(disposing);
+        }
     }
 }
