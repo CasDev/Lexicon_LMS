ad2eeb169e3917fd5fe1c12e7fbd31ac622cceae
Ari Kylmänen <ari.kylmanen@comhem.se>
2016-06-28 15:47:12 +0200

    Translation error messages to swedish

--- LMS/Models/AccountViewModels.cs
+++ LMS/Models/AccountViewModels.cs
@@ -49,20 +49,20 @@ namespace LMS.Models
 
     public class LoginViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "E-post måste fyllas i")]
         //[Display(Name = "E-post")]
         [Display(Name = "Email")]
-        [EmailAddress]
+        [EmailAddress(ErrorMessage ="E-post har felaktigt format")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Lösenord måste fyllas i")]
         [DataType(DataType.Password)]
         //[Display(Name = "Lösenord")]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
         //[Display(Name = "Remember me?")]
-        [Display(Name = "Remember me?")]
+        [Display(Name = "Kom ihåg mig?")]
         public bool RememberMe { get; set; }
     }
 
