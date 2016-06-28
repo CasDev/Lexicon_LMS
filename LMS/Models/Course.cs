3150bd3be0a59a88fa6efdbee52b4d74f314440d
kaffeutvecklare <kaffeutvecklare@gmail.com>
2016-06-28 09:56:06 +0200

    Added some meta-data to Swedish, and also extracted the list of users in a Course to their own partial view

--- LMS/Models/Course.cs
+++ LMS/Models/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,17 @@ namespace LMS.Models
     public class Course
     {
         public int Id { get; set; }
+
+        [Display(Name = "Namn")]
         public string Name { get; set; }
+
+        [Display(Name = "Beskrivning")]
         public string Description { get; set; }
+
+        [Display(Name = "Startdatum")]
         public DateTime StartDate { get; set; }
+
+        [Display(Name = "Slutdatum")]
         public DateTime EndDate { get; set; }
 
         public virtual ICollection<User> Users { get; set; }
