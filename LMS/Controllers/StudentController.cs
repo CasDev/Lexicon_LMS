99529fc47af7e8e98e83257e8794c24253e73624
kaffeutvecklare <kaffeutvecklare@gmail.com>
2016-06-30 11:46:41 +0200

    Added a HttpGet and HttpPost for Assignment. The Post doesn't save file yet, as it hasn't been fully implemented as off yet

--- LMS/Controllers/StudentController.cs
+++ LMS/Controllers/StudentController.cs
@@ -2,6 +2,7 @@
 using LMS.Models.DataAccess;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -118,13 +119,39 @@ namespace LMS.Controllers
             return View(course);
         }
 
+        [HttpPost]
+        public ActionResult Assignment(int? id, HttpPostedFileBase file)
+        {
+            // Verify that the user selected a file
+            if (file != null && file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                // TODO: make this work
+//                var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+//                file.SaveAs(path);
+            } else
+            {
+                ModelState.AddModelError("", "En fil med innehåll måste erhållas");
+            }
+            // redirect back to the index action to show the form once again
+//            return RedirectToAction("Index");
+
+            return View("~/Views/Student/Activity.cshtml");
+        }
+
+        [HttpGet]
+        public ActionResult Assignment(int? id)
+        {
+            return Redirect("~/Student/Activity" + (id != null ? "?id="+ id : ""));
+        }
 
         [Authorize(Roles = "Student")]
         public ActionResult Activity(int? id)
         {
             //TODO: vad händer om id är null?
             // TODO: Hämta aktivitet
-/*            Activity activity = new Activity();
+            Activity activity = new Activity();
+            /*
             if (course == null)
             {
                 return View("~/Views/Student/NoKnown.cshtml");
