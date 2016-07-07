2854451ce1118d4eb31c6b5ccf9d7ccbeaa7bd9f
kaffeutvecklare <kaffeutvecklare@gmail.com>
2016-07-07 16:58:44 +0200

    Removed some of the methods we aren't going to use

--- LMS/Controllers/TeacherController.cs
+++ LMS/Controllers/TeacherController.cs
@@ -236,51 +236,10 @@ namespace LMS.Controllers
             return Redirect("~/Teacher/");
         }
 
-        // GET: Teacher
         public ActionResult Index()
         {
             var users = db.Users.Include(u => u.Courses);
             return View(users.ToList());
-        }
-
-        // GET: Teacher/Details/5
-        public ActionResult Details(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            User user = db.Users.Find(id);
-            if (user == null)
-            {
-                return HttpNotFound();
-            }
-            return View(user);
-        }
-
-        // GET: Teacher/Create
-        public ActionResult Create()
-        {
-            ViewBag.CoursesId = new SelectList(db.Courses, "Id", "Name");
-            return View();
-        }
-
-        // POST: Teacher/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,CoursesId,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName")] User user)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Users.Add(user);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.CoursesId = new SelectList(db.Courses, "Id", "Name", user.CoursesId);
-            return View(user);
         }
 
         [HttpGet]
@@ -322,68 +281,7 @@ namespace LMS.Controllers
         {
             return View();
         }
-
-
-        // GET: Teacher/Edit/5
-        public ActionResult Edit(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            User user = db.Users.Find(id);
-            if (user == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.CoursesId = new SelectList(db.Courses, "Id", "Name", user.CoursesId);
-            return View(user);
-        }
-
-
-        // POST: Teacher/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,CoursesId,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName")] User user)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(user).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.CoursesId = new SelectList(db.Courses, "Id", "Name", user.CoursesId);
-            return View(user);
-        }
-
-        // GET: Teacher/Delete/5
-        public ActionResult Delete(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            User user = db.Users.Find(id);
-            if (user == null)
-            {
-                return HttpNotFound();
-            }
-            return View(user);
-        }
-
-        // POST: Teacher/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(string id)
-        {
-            User user = db.Users.Find(id);
-            db.Users.Remove(user);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
+        
         protected override void Dispose(bool disposing)
         {
             if (disposing)
