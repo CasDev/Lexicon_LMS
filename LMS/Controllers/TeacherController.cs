ca50f0ff49ec66a88e0e151d78fef7ec2a0f7345
MarieHansson <mariehansson10@hotmail.com>
2016-07-07 14:02:03 +0200

    Efter lunch 14.00. Modellen Activitet skapad.

--- LMS/Controllers/TeacherController.cs
+++ LMS/Controllers/TeacherController.cs
@@ -235,6 +235,7 @@ namespace LMS.Controllers
             return View(user);
         }
 
+
         // POST: Teacher/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
