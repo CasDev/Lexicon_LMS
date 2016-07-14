4aa8a1b762d37da459e5c91d9ab9f7761224d357
kaffeutvecklare <kaffeutvecklare@gmail.com>
2016-07-14 10:04:33 +0200

    Well, now we can add new students to a course anyway so that is a big leap into something :)

--- LMS/Controllers/StudentController.cs
+++ LMS/Controllers/StudentController.cs
@@ -285,6 +285,7 @@ namespace LMS.Controllers
             bool _sort = (sort != null && sort == "FirstName" ? false : true);
 
             course.Users = (course.Users != null ? course.Users : new List<User>());
+            course.Users = course.Users.Where(s => s.IsStudent()).ToList();
             if (_sort)
             {
                 course.Users = course.Users.OrderBy(u => u.LastName).ToList();
