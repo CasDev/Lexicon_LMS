48e542ebb241d967fcaaa5a1ea837cf29dc50844
kaffeutvecklare <kaffeutvecklare@gmail.com>
2016-07-15 14:31:58 +0200

    Added a course to the seed()-method

--- LMS/Migrations/Configuration.cs
+++ LMS/Migrations/Configuration.cs
@@ -66,11 +66,19 @@ namespace LMS.Migrations
             uManager.AddToRole(user.Id, "Student");
             uManager.Update(user);
 
+            // utgågna kursen .NET
             Course Course = new Course { Name = ".NET, Våren -15", Description = "En utbildning i .NET C#, .NET MVC5, Bootstrap, AngularJS, etc. etc.", StartDate = new DateTime(2015, 2, 16), EndDate = new DateTime(2015, 8, 15, 23, 59, 0), Users = new List<User>() };
             context.Courses.AddOrUpdate(x => x.Name,
                 Course);
             context.SaveChanges();
 
+            // utgågna kursen Java
+            Course = new Course { Name = "Java, Våren -15", Description = "En utbildning i Java, Spring-framework MVC, Bootstrap, AngularJS, etc. etc.", StartDate = new DateTime(2015, 2, 3), EndDate = new DateTime(2015, 8, 19, 23, 59, 0), Users = new List<User>() };
+            context.Courses.AddOrUpdate(x => x.Name,
+                Course);
+            context.SaveChanges();
+
+            // "pågående" kursen .NET
             Course = new Course { Name = ".NET, Våren -16", Description = "En utbildning i .NET C#, .NET MVC5, Bootstrap, AngularJS, etc. etc.", StartDate = new DateTime(2016, 2, 16), EndDate = new DateTime(2016, 8, 15, 23, 59, 0), Users = new List<User>() };
             Course.Users.Add(user);
             user = uManager.FindByName("castell_john@hotmail.com");
@@ -115,8 +123,11 @@ namespace LMS.Migrations
                     context.Users.AddOrUpdate(u => u.UserName, user);
                 }
             }
-            
-            Module Module = new Module { Name = "C#", Description = "Learning C#", StartDate = new DateTime(2016, 3, 2), EndDate = new DateTime(2016, 3, 28, 23, 59, 0) };
+            context.Courses.AddOrUpdate(x => x.Name,
+                Course);
+            context.SaveChanges();
+
+            Module Module = new Module { Name = "C#", Description = "Learning C#", StartDate = new DateTime(2016, 3, 2), EndDate = new DateTime(2016, 3, 28, 23, 59, 0), CourseId = Course.Id };
             Module.CourseId = Course.Id;
             context.Modules.AddOrUpdate(m => m.Name,
                 Module);
@@ -135,7 +146,7 @@ namespace LMS.Migrations
                 Activity);
             context.SaveChanges();
 
-            Module = new Module { Name = "SQL Server", Description = "Learning how MS SQL Server works", StartDate = new DateTime(2016, 3, 1), EndDate = new DateTime(2016, 5, 13, 23, 59, 0) };
+            Module = new Module { Name = "SQL Server", Description = "Learning how MS SQL Server works", StartDate = new DateTime(2016, 3, 1), EndDate = new DateTime(2016, 5, 13, 23, 59, 0), CourseId = Course.Id };
             Module.CourseId = Course.Id;
             context.Modules.AddOrUpdate(m => m.Name,
                 Module);
@@ -146,7 +157,7 @@ namespace LMS.Migrations
                 Activity);
             context.SaveChanges();
 
-            Module = new Module { Name = "Identity", Description = "Går igenom Identity och Individual User Interface(?) med gästföreläsare", StartDate = new DateTime(2016, 5, 16), EndDate = new DateTime(2016, 5, 18, 23, 59, 0) };
+            Module = new Module { Name = "Identity", Description = "Går igenom Identity och Individual User Interface(?) med gästföreläsare", StartDate = new DateTime(2016, 5, 16), EndDate = new DateTime(2016, 5, 18, 23, 59, 0), CourseId = Course.Id };
             Module.CourseId = Course.Id;
             context.Modules.AddOrUpdate(m => m.Name,
                 Module);
@@ -159,7 +170,7 @@ namespace LMS.Migrations
                 Activity);
             context.SaveChanges();
 
-            Module = new Module { Name = "Övningstillfällen", Description = "Öva på diverse", StartDate = new DateTime(2016, 2, 16), EndDate = new DateTime(2016, 8, 15, 23, 59, 0) };
+            Module = new Module { Name = "Övningstillfällen", Description = "Öva på diverse", StartDate = new DateTime(2016, 2, 16), EndDate = new DateTime(2016, 8, 15, 23, 59, 0), CourseId = Course.Id };
             Module.CourseId = Course.Id;
             context.Modules.AddOrUpdate(m => m.Name,
                 Module);
@@ -176,7 +187,7 @@ namespace LMS.Migrations
                 Activity);
             context.SaveChanges();
 
-            Module = new Module { Name = "Slutprojekt", Description = "Jobba på slutprojekt i grupp", StartDate = new DateTime(2016, 7, 20), EndDate = new DateTime(2016, 8, 10, 23, 59, 0) };
+            Module = new Module { Name = "Slutprojekt", Description = "Jobba på slutprojekt i grupp", StartDate = new DateTime(2016, 7, 20), EndDate = new DateTime(2016, 8, 10, 23, 59, 0), CourseId = Course.Id };
             Module.CourseId = Course.Id;
             context.Modules.AddOrUpdate(m => m.Name,
                 Module);
@@ -192,6 +203,112 @@ namespace LMS.Migrations
             context.Activities.AddOrUpdate(x => x.Name,
                 Activity);
             Activity = new Activity { Name = "Slutprojekt", Description = "En inlämningsuppgift för slutprojektet", StartDate = null, EndDate = null, Deadline = new DateTime(2016, 8, 10, 17, 0, 0), ModuleId = Module.Id, Type = "Practice" };
+            context.Activities.AddOrUpdate(x => x.Name,
+                Activity);
+            context.SaveChanges();
+
+            context.Courses.AddOrUpdate(x => x.Name,
+                Course);
+
+            // "pågående" kursen Java
+            Course = new Course { Name = "Java, Våren -16", Description = "En utbildning i Java, Spring-framework, Bootstrap, AngularJS, etc. etc.", StartDate = new DateTime(2016, 2, 13), EndDate = new DateTime(2016, 8, 15, 17, 59, 0), Users = new List<User>() };
+            context.Courses.AddOrUpdate(x => x.Name,
+                Course);
+
+            foreach (User _user in new User[] {
+                new User { Email = "tommy@castell.se", UserName = "tommy@castell.se", FirstName = "Tommy", LastName = "Castell" },
+                new User { Email = "monica@castell.se", UserName = "monica@castell.se", FirstName = "Monica", LastName = "Castell" },
+                new User { Email = "veronica.brown@sheff.uni.co.uk", UserName = "veronica.brown@sheff.uni.co.uk", FirstName = "Veronica", LastName = "Brown" },
+            })
+            {
+                uManager.Create(_user, "password");
+                user = uManager.FindByName(_user.UserName);
+
+                uManager.AddToRole(user.Id, "Student");
+                Course.Users.Add(user);
+                if (user.CoursesId == null || user.CoursesId <= 0)
+                {
+                    user.CoursesId = Course.Id;
+                    context.Users.AddOrUpdate(u => u.UserName, user);
+                }
+            }
+
+            Module = new Module { Name = "Java", Description = "Learning Java", StartDate = new DateTime(2016, 3, 2), EndDate = new DateTime(2016, 3, 28, 23, 59, 0), CourseId = Course.Id };
+            Module.CourseId = Course.Id;
+            context.Modules.AddOrUpdate(m => m.Name,
+                Module);
+            context.SaveChanges();
+
+            // Ändrade i Seed:en här så att aktiviteterna ligger mer i "synch" till vilka moduler dom tillhör
+
+            Activity = new Activity { Name = "Pluralsight ( Java 8 )", Description = "Se videos av XXX XXX på ämnet Java", StartDate = null, EndDate = null, Deadline = Module.EndDate, ModuleId = Module.Id, Type = "E-learning" };
+            context.Activities.AddOrUpdate(x => x.Name,
+                Activity);
+            Activity = new Activity { Name = "Pluralsight ( Differences in Java 7 - Java 8 )", Description = "Se videos av XXX XXX på ämnet 'skillnader mellan Jav 7 och Java 8'", StartDate = null, EndDate = null, Deadline = Module.EndDate, ModuleId = Module.Id, Type = "E-learning" };
+            context.Activities.AddOrUpdate(x => x.Name,
+                Activity);
+            Activity = new Activity { Name = "Öva på Java", Description = "Java-övningar", StartDate = new DateTime(2016, 3, 28, 8, 30, 0), EndDate = new DateTime(2016, 3, 28, 17, 0, 0), Deadline = new DateTime(2016, 3, 28, 17, 0, 0), ModuleId = Module.Id, Type = "Practice" };
+            context.Activities.AddOrUpdate(x => x.Name,
+                Activity);
+            context.SaveChanges();
+
+            Module = new Module { Name = "MySQL", Description = "Learning how MySQL works", StartDate = new DateTime(2016, 3, 1), EndDate = new DateTime(2016, 5, 13, 23, 59, 0), CourseId = Course.Id };
+            Module.CourseId = Course.Id;
+            context.Modules.AddOrUpdate(m => m.Name,
+                Module);
+            context.SaveChanges();
+
+            Activity = new Activity { Name = "Learning SQL", Description = "SQL-övningar", StartDate = null, EndDate = null, Deadline = new DateTime(2016, 5, 13, 17, 0, 0), ModuleId = Module.Id, Type = "Practice" };
+            context.Activities.AddOrUpdate(x => x.Name,
+                Activity);
+            context.SaveChanges();
+
+            Module = new Module { Name = "Inloggning", Description = "Går igenom hur man loggar in och authenticeras användare mot tredje parts inloggningar", StartDate = new DateTime(2016, 5, 16), EndDate = new DateTime(2016, 5, 18, 23, 59, 0), CourseId = Course.Id };
+            Module.CourseId = Course.Id;
+            context.Modules.AddOrUpdate(m => m.Name,
+                Module);
+
+            Activity = new Activity { Name = "Föreläsning om social media API", Description = "API-föreläsning", StartDate = new DateTime(2016, 5, 16), EndDate = new DateTime(2016, 5, 17, 17, 30, 0), Deadline = null, ModuleId = Module.Id, Type = "Lecture" };
+            context.Activities.AddOrUpdate(x => x.Name,
+                Activity);
+            Activity = new Activity { Name = "Skapa ett project för att hämta information från Facebook", Description = "Hämta social media information via ett öppet API", StartDate = null, EndDate = null, Deadline = null, ModuleId = Module.Id, Type = "Practice" };
+            context.Activities.AddOrUpdate(x => x.Name,
+                Activity);
+            context.SaveChanges();
+
+            Module = new Module { Name = "Övningstillfällen ( Java -16 ) ", Description = "Öva på diverse", StartDate = new DateTime(2016, 2, 16), EndDate = new DateTime(2016, 8, 15, 23, 59, 0), CourseId = Course.Id };
+            Module.CourseId = Course.Id;
+            context.Modules.AddOrUpdate(m => m.Name,
+                Module);
+            context.SaveChanges();
+
+            Activity = new Activity { Name = "Övning i HTML 5", Description = "HTML 5-övning. Skapa en enkel sida", StartDate = null, EndDate = new DateTime(2016, 4, 18, 17, 0, 0), Deadline = new DateTime(2016, 4, 18, 15, 30, 0), ModuleId = Module.Id, Type = "Practice" };
+            context.Activities.AddOrUpdate(x => x.Name,
+                Activity);
+            Activity = new Activity { Name = "Övningar inom CSS 3", Description = "Övningar i CSS3", StartDate = null, EndDate = new DateTime(2016, 6, 21, 17, 0, 0), Deadline = new DateTime(2016, 6, 21, 15, 30, 0), ModuleId = Module.Id, Type = "Practice" };
+            context.Activities.AddOrUpdate(x => x.Name,
+                Activity);
+            Activity = new Activity { Name = "JavaScript / jQuery", Description = "En enkel applikation som hämtar en HTML-node och modifierar text", StartDate = null, EndDate = new DateTime(2016, 7, 2, 17, 0, 0), Deadline = new DateTime(2016, 7, 2, 15, 30, 0), ModuleId = Module.Id, Type = "Practice" };
+            context.Activities.AddOrUpdate(x => x.Name,
+                Activity);
+            context.SaveChanges();
+
+            Module = new Module { Name = "Slutprojekt ( Java -16 )", Description = "Jobba på slutprojekt i grupp", StartDate = new DateTime(2016, 7, 20), EndDate = new DateTime(2016, 8, 10, 23, 59, 0), CourseId = Course.Id };
+            Module.CourseId = Course.Id;
+            context.Modules.AddOrUpdate(m => m.Name,
+                Module);
+            context.SaveChanges();
+
+            Activity = new Activity { Name = "Sprint 1 ( Java -16 )", Description = "Första sprinten i SCRUM-arbetet för slutprojektet", StartDate = new DateTime(2016, 7, 20), EndDate = new DateTime(2016, 7, 27, 17, 0, 0), Deadline = null, ModuleId = Module.Id, Type = "Practice" };
+            context.Activities.AddOrUpdate(x => x.Name,
+                Activity);
+            Activity = new Activity { Name = "Sprint 2 ( Java -16 )", Description = "Andra sprinten i SCRUM-arbete för slutprojektet", StartDate = new DateTime(2016, 7, 28), EndDate = new DateTime(2016, 8, 5, 17, 0, 0), Deadline = null, ModuleId = Module.Id, Type = "Practice" };
+            context.Activities.AddOrUpdate(x => x.Name,
+                Activity);
+            Activity = new Activity { Name = "Sprint 3 ( Java -16 )", Description = "Tredje sprinten i SCRUM-arbete för slutprojektet", StartDate = new DateTime(2016, 8, 6), EndDate = new DateTime(2016, 8, 10, 17, 0, 0), Deadline = null, ModuleId = Module.Id, Type = "Practice" };
+            context.Activities.AddOrUpdate(x => x.Name,
+                Activity);
+            Activity = new Activity { Name = "Slutprojekt ( Java -16 )", Description = "En inlämningsuppgift för slutprojektet", StartDate = null, EndDate = null, Deadline = new DateTime(2016, 8, 10, 17, 0, 0), ModuleId = Module.Id, Type = "Practice" };
             context.Activities.AddOrUpdate(x => x.Name,
                 Activity);
             context.SaveChanges();
